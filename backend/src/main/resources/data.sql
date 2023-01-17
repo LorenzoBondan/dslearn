7e57b75d40fec5dd37f62945500492175dd89292
LorenzoBondan <lbbondan@gmail.com>
2023-01-17 15:51:34 -0300

    Resource

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -19,6 +19,12 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-07-13T20:50:07Z', 1 );
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-07-13T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-07-13T20:50:07Z', 1 );
 
+
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('First Notification', TIMESTAMP WITH TIME ZONE '2020-07-13T03:00:00Z', false, 'Route', 1);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Account Created', TIMESTAMP WITH TIME ZONE '2022-10-12T03:00:00Z', true, 'Route', 2);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Suspected Login Detected', TIMESTAMP WITH TIME ZONE '2022-12-25T03:00:00Z', false, 'Route', 3);
+
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Rota de aulas HTML' , 'Trilha principal do curso' , 1 , 'http://imagem.com' , 1 , 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Fórum' , 'Tire suas dúvidas' , 2 , 'http://imagem.com' , 2 , 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives' , 'Lives exclusivas da turma' , 3 , 'http://imagem.com' , 0 , 1);
